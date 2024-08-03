b15cb7294d1e564e280267cd6d5ba02fd519b8de
saye <saye.ghadimi79@gmali.com>
2024-08-03 22:26:58 +0300

    work-4

--- src/main/java/controller/StudentController.java
+++ src/main/java/controller/StudentController.java
@@ -1,2 +1,26 @@
-package controller;public class StudentController {
+package controller;
+
+import model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import repository.StudentRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("Student")
+
+
+
+public class StudentController {
+    @Autowired
+    StudentRepository studentRepository;
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+
 }
--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -1,2 +1,18 @@
-package model;public class student {
+package model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@Table(name="studenttb")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Long id;
+    private String name;
+    private String email;
+
 }
